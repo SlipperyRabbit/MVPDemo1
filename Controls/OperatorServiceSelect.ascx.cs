d431c5f1c4dd8423fd16537f1854d8f0364cbc87
David Lambert <dlambert@uoregon.edu>
2012-05-26 14:55:59 -0700

    It works with the interface instead of a default IView instance.

--- Controls/OperatorServiceSelect.ascx.cs
+++ Controls/OperatorServiceSelect.ascx.cs
@@ -8,11 +8,21 @@ using WebFormsMvp;
 using WebFormsMvp.Web;
 using MVPDemo1.Logic.Models;
 using MVPDemo1.Logic.Presentation;
+using MVPDemo1.Logic.Views;
+using MVPDemo1.Logic.OperatorDataServices;
 
 namespace MVPDemo1.Controls
 {
 	[PresenterBinding(typeof(OperatorDisplayPresenter))]
-	public partial class OperatorServiceSelect : MvpUserControl<OperatorDisplayModel>
+	public partial class OperatorServiceSelect : MvpUserControl<OperatorDisplayModel>, IOperatorDisplayView
 	{
+		public dsOperator Operator { get; set; }
+		public bool IsVisible { get; set; }
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			Operator = Model.Operator;
+			IsVisible = Model.IsVisible;
+		}
 	}
 }
